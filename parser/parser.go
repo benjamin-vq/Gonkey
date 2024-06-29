9bbccf14084ededea4b7af12fb7a0d394826a87a
benjamin-vq <b.villalobosq94@gmail.com>
2024-06-28 21:57:21 -0400

    feat: Parse let statements

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"github.com/benja-vq/gonkey/ast"
 	"github.com/benja-vq/gonkey/lexer"
 	"github.com/benja-vq/gonkey/token"
+	"log"
 )
 
 type Parser struct {
@@ -32,5 +33,69 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	if !p.expectPeek(token.IDENT) {
+		log.Printf("Let statement expects next token to be an identifier, got %+v",
+			p.peekToken)
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		log.Printf("Identifier expects an assignment, got %+v",
+			p.peekToken)
+		return nil
+	}
+
+	// TODO: Skipping expressions for now, just find a semicolon.
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currTokenIs(tt token.TokenType) bool {
+	return p.currToken.Type == tt
+}
+
+func (p *Parser) peekTokenIs(tt token.TokenType) bool {
+	return p.peekToken.Type == tt
+}
+
+func (p *Parser) expectPeek(tt token.TokenType) bool {
+	if p.peekTokenIs(tt) {
+		p.nextToken()
+		return true
+	}
+
+	return false
 }
